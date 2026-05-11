1177f752c406d36b3a8889afd4c517c29fcc2390
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-05-11 01:08:44 -0300

    Match move category button to image ratio

--- app/src/main/java/com/example/appfightsmart/GameSetupScreen.kt
+++ app/src/main/java/com/example/appfightsmart/GameSetupScreen.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -421,13 +421,13 @@ private fun MoveCategoryCard(
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(78.dp)
+                .aspectRatio(3f)
         ) {
             Image(
                 painter = painterResource(id = R.drawable.button),
                 contentDescription = null,
                 modifier = Modifier.matchParentSize(),
-                contentScale = ContentScale.Crop
+                contentScale = ContentScale.FillBounds
             )
 
             if (!selected) {
