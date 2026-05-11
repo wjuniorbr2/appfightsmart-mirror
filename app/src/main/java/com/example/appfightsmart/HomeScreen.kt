6241cd4867d8f8196f0ae4d0da24624bac826f31
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-05-11 02:11:40 -0300

    Fix rememberSaveable import

--- app/src/main/java/com/example/appfightsmart/HomeScreen.kt
+++ app/src/main/java/com/example/appfightsmart/HomeScreen.kt
@@ -27,7 +27,7 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
