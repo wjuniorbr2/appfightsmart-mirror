4241a3b3534e4223fddaa206a21eec79a7ca27ab
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-05-18 01:10:58 -0300

    Move calibration entry into settings

--- app/src/main/java/com/example/appfightsmart/SettingsScreen.kt
+++ app/src/main/java/com/example/appfightsmart/SettingsScreen.kt
@@ -1,25 +1,40 @@
 package com.example.appfightsmart
 
-import androidx.navigation.NavHostController
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.*
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import java.util.Locale
-import androidx.appcompat.app.AppCompatDelegate
 
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(navController: NavHostController) {
     val context = LocalContext.current
     val sharedPreferences = remember { context.getSharedPreferences("AppSettings", Context.MODE_PRIVATE) }
 
@@ -27,23 +42,12 @@ fun SettingsScreen() {
     var isDarkMode by remember { mutableStateOf(sharedPreferences.getBoolean("dark_mode", false)) }
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Theme Section
-        Text(
-            text = stringResource(id = R.string.theme),
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
+        Text(text = stringResource(id = R.string.theme), style = MaterialTheme.typography.titleMedium, modifier = Modifier.padding(bottom = 8.dp))
         Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
             Text(text = "Normal Mode", modifier = Modifier.padding(end = 16.dp))
             Switch(
                 checked = isDarkMode,
@@ -56,28 +60,21 @@ fun SettingsScreen() {
             Text(text = "Dark Mode", modifier = Modifier.padding(start = 16.dp))
         }
 
-        // Language Section
-        Text(
-            text = stringResource(id = R.string.language),
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = stringResource(id = R.string.language), style = MaterialTheme.typography.titleMedium, modifier = Modifier.padding(bottom = 8.dp))
         Spacer(modifier = Modifier.height(8.dp))
         val languages = listOf("English" to "en", "Português" to "pt")
         languages.forEach { (langName, langCode) ->
             Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .selectable(
-                        selected = (langCode == language),
-                        onClick = {
-                            language = langCode
-                            sharedPreferences.edit().putString("language", langCode).apply()
-                            setAppLanguage(context, langCode)
-                            restartActivity(context) // Restart the activity to apply the language change
-                        }
-                    )
-                    .padding(8.dp),
+                modifier = Modifier.fillMaxWidth().selectable(
+                    selected = (langCode == language),
+                    onClick = {
+                        language = langCode
+                        sharedPreferences.edit().putString("language", langCode).apply()
+                        setAppLanguage(context, langCode)
+                        restartActivity(context)
+                    }
+                ).padding(8.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 RadioButton(
@@ -86,28 +83,16 @@ fun SettingsScreen() {
                         language = langCode
                         sharedPreferences.edit().putString("language", langCode).apply()
                         setAppLanguage(context, langCode)
-                        restartActivity(context) // Restart the activity to apply the language change
+                        restartActivity(context)
                     }
                 )
                 Text(text = langName, modifier = Modifier.padding(start = 8.dp))
             }
         }
 
-        // Divider between Language and Theme
-        HorizontalDivider(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp),
-            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
-        )
+        HorizontalDivider(modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp), color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f))
 
-        // Calibration Button (Placeholder)
-        Button(
-            onClick = {
-                // TODO: Implement calibration logic later
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
+        Button(onClick = { navController.navigate(Screen.Calibration.route) }, modifier = Modifier.fillMaxWidth()) {
             Text(text = stringResource(id = R.string.calibrate_sensor))
         }
     }
@@ -122,13 +107,7 @@ private fun setAppLanguage(context: Context, languageCode: String): Context {
 }
 
 private fun setAppTheme(context: Context, isDarkMode: Boolean) {
-    if (isDarkMode) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-    } else {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-    }
-
-    // Restart the activity to apply the theme change
+    AppCompatDelegate.setDefaultNightMode(if (isDarkMode) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO)
     restartActivity(context)
 }
 
