1f0d8d075662b50da890c2aa43eb73e165ab6cea
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-05-19 06:06:55 -0300

    Add bag preview placeholder

---
+++ app/src/main/java/com/example/appfightsmart/BagPreviewPlaceholder.kt
@@ -0,0 +1,9 @@
+package com.example.appfightsmart
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BagPreviewPlaceholder() {
+    Text("Bag preview placeholder")
+}
